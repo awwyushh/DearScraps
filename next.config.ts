5560176fd426c50c4a1a9525d385c176215e726d
awwyushh <awwwyushh@gmail.com>
2025-03-02 13:25:42 +0530

    p

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing',
+        permanent: true, // Set to false if you want a temporary redirect
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
